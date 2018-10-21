e3962f989cb5ba034ec65a56afcc3a6e8dda55e8
devMarshall <onyendo52@gmail.com>
2018-10-21 14:22:53 +0100

    created models and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,7 @@ class CreateUsersTable extends Migration
             $table->string('handle_github')->nullable();
             $table->string('handle_instagram')->nullable();
             $table->string('phone')->nullable();
+            $table->string('website')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
