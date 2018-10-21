017006981687c9a51a2b1aed7f1b15f7edba257b
devMarshall <onyendo52@gmail.com>
2018-10-21 14:20:59 +0100

    created models and migrations

---
+++ app/child_skill.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class child_skill extends Model
+{
+    //
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name_f');
+            $table->string('name_l');
+            $table->string('name_display')->nullable();
+            $table->string('img_src')->nullable();
+            $table->string('handle_twitter')->nullable();
+            $table->string('handle_facebook')->nullable();
+            $table->string('handle_behance')->nullable();
+            $table->string('handle_github')->nullable();
+            $table->string('handle_instagram')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
